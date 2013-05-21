363447de1b619e82c68b19e87a5e288adba80f09
Benjamin Koether <ben@koether.ca>
2013-05-21 14:10:12 -0700

    Fix for locaStorage

--- sites/all/themes/grey6/script.js
+++ sites/all/themes/grey6/script.js
@@ -619,3 +619,14 @@ if (typeof console == "undefined") {
         log: function () {}
     };
 }
+
+// Get around browsers that don't support localStorage
+if(typeof window.localStorage == 'undefined') {
+  window.localStorage = {
+    _data       : {},
+    setItem     : function(id, val) { return this._data[id] = String(val); },
+    getItem     : function(id) { return this._data.hasOwnProperty(id) ? this._data[id] : undefined; },
+    removeItem  : function(id) { return delete this._data[id]; },
+    clear       : function() { return this._data = {}; }
+  };
+}
