966341fdfe092d03a4a803e93be65b622e16ae04
Benjamin Koether <ben@koether.ca>
2014-07-24 16:43:34 -0700

    Up the version number

--- sites/all/themes/grey6/remittance_header_combined.php
+++ sites/all/themes/grey6/remittance_header_combined.php
@@ -50,7 +50,7 @@ $provinces = array(
   var formType = '<?php echo $form_type; ?>';
 
   var currentUser = <?php echo $user->uid; ?>;
-  var currentVersion = '<?php echo $form_type; ?>_8';
+  var currentVersion = '<?php echo $form_type; ?>_9';
 
   var currentRates;
   var oemLevel;
